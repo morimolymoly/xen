aeec9379e6b5ca6ef23bcf34b582d7fed6111483
morimolymoly <morimolymoly@gmail.com>
2018-10-04 19:16:04 +0900

    fix bug

--- xen/arch/x86/hvm/vmx/vmx.c
+++ xen/arch/x86/hvm/vmx/vmx.c
@@ -1887,7 +1887,7 @@ static void vmx_inject_event(const struct x86_event *event)
 
     case TRAP_page_fault:
         ASSERT(_event.type == X86_EVENTTYPE_HW_EXCEPTION);
-        printk("cr2:%llx rip:%llx\n", _event.cr2, curr->arch.hvm_vcpu.rip):
+        printk("cr2:%llx rip:%llx\n", _event.cr2, curr->arch.hvm_vcpu.rip);
         curr->arch.hvm_vcpu.guest_cr[2] = _event.cr2;
         curr->arch.hvm_vcpu.guest_cr[2] = curr->arch.hvm_vcpu.rip;
         break;
